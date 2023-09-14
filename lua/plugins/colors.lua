9d26db0e3f9a506046d5cd6bf4c281e45994a8db
Yamis4n <evertonvsf2@gmail.com>
2023-09-14 08:35:07 -0300

    add validation to ChooseTheme function

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -3,17 +3,23 @@ COLORS = {"catppuccin-latte", "catppuccin-frappe", "catppuccin-macchiato", "catp
 local lualine = require("lualine")
 
 function ChooseTheme(theme)
+
   theme = theme or "dark"
+  theme = string.lower(theme)
 
   if (theme == "dark") then
     vim.o.background = "dark"
     vim.cmd.colorscheme("dracula")
     lualine.setup({options={theme = "dracula"}})
-  end
-  if (theme == "light") then
+  elseif (theme == "light") then
     vim.o.background = "light"
     vim.cmd.colorscheme("gruvbox")
     lualine.setup({options={theme = "gruvbox_light"}})
+  
+  else
+    print("Please choose one of this themes:")
+    print("   - Light")
+    print("   - Dark")
   end
 end
 
